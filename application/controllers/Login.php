c8eda33d76ef2a9b4ef21dce3cb5ac1e8b7ef0a3
Milton Otavo <milton.otavo@gmail.com>
2017-06-30 13:43:11 -0500

    Session

---
+++ application/controllers/Login.php
@@ -0,0 +1,45 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+
+	public function index()
+	{
+		$this->session->sess_destroy();
+		$this->load->view('login');
+	}
+
+	public function form()
+	{
+		$inputUser  = $this->input->post('usuario');
+		$inpurPass  = $this->input->post('contrasena');
+
+		$this->load->model('user');
+		$fila=$this->user->getUser($inputUser);
+
+		if ($fila!=null) {
+			if ($fila->PASS == $inpurPass ) {
+				$data = array (
+					'user'  => $inputUser,
+					'id'    => 0,
+					'login' => true
+					);
+				$this->session->set_userdata($data);
+			} else {
+				header("Location:" . base_url());
+			}
+		} else {
+			header("Location:" . base_url());
+		}
+
+	/*	
+		
+
+	echo $this->session->userdata('id'); */
+
+}
+
+
+
+}
